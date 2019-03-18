3ae3f350cfeff12dc68e68d4af072b7590031b22
lilvina <davinaleo2007@yahoo.com>
2019-03-18 15:59:16 -0700

    put and delete works, MVP completed

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ server.get('/api/users', (req, res) => {
     res.status(500).json({ message: 'The users information cannot be retrieved.' })
   })
 })
+
 server.get('/api/users/:id', (req, res) => {
   const {id} = req.params
 
@@ -40,6 +41,39 @@ server.get('/api/users/:id', (req, res) => {
   }).catch(err => {
     res.status(500).json({ message: 'The user information could not be retrieved.' })
   })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+  const {id} = req.params
+
+  db.remove(id).then(user => {
+    if(user) {
+      res.status(200).json(user)
+    } else {
+      res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+    }
+  }).catch(err => {
+    res.status(500).json({ message: 'The user could not be removed.' })
+  })
+})
+
+server.put('/api/users/:id', (req, res) => {
+  const {id} = req.params
+  const changes = req.body
+
+  if(changes.name && changes.bio) {
+    db.update(id, changes).then(user => {
+      if(user) {
+        res.status(200).json(user)
+      } else {
+        res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+      }
+    }).catch(err => {
+      res.status(500).json({ message: 'The user information could not be modified.' })
+    })
+  } else {
+    res.status(400).json({ message: 'Please provide name and bio for the user.' })
+  }
 })
 
 server.listen(4000, () => {
