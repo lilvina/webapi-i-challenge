fbe22a688119c90565e812f6cc7c5be63671cad9
lilvina <davinaleo2007@yahoo.com>
2019-03-18 15:19:05 -0700

    both get requests work

--- index.js
+++ index.js
@@ -22,10 +22,23 @@ server.post('/api/users', (req, res) => {
 })
 
 server.get('/api/users', (req, res) => {
-  db.users.find().then(user => {
-    res.status(200).json(user)
+  db.find().then(users => {
+    res.status(200).json(users)
   }).catch(err => {
     res.status(500).json({ message: 'The users information cannot be retrieved.' })
+  })
+})
+server.get('/api/users/:id', (req, res) => {
+  const {id} = req.params
+
+  db.findById(id).then(user => {
+    if(user) {
+      res.status(200).json(user)
+    } else {
+      res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+    }
+  }).catch(err => {
+    res.status(500).json({ message: 'The user information could not be retrieved.' })
   })
 })
 
